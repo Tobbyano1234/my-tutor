da6c246d145ab343e6adf4ab77b124a0b490016c
AdegbesanOluwat <>
2022-05-31 16:00:56 +0000

    complete love check function

--- tutor.js
+++ tutor.js
@@ -1,11 +1,11 @@
 function luvcheck(petals1, petals2) {
   if (petals1%2 == 0 && petals2%2 != 0) {
     return true;
-  } else (petals2%2 == 0 && petals1%2 != 0) {
+  } else if (petals2%2 == 0 && petals1%2 != 0) {
+    return true;
+  } else {
     return false;
-  // } else {
-  //   return false;
-  // }fgdfgfhdgghjmthrgfsghregf
+  }
 }
 
 console.log(luvcheck(2,1));
